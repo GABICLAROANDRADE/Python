e25b0f71ce9c500f68f5b4925faa8e7adb34591d
GABICLAROANDRADE <gabrielaclaroandrade2010@gmail.com>
2024-03-12 11:02:51 -0300

    Primeira aula

--- Alo1.py
+++ Alo1.py
@@ -1,1 +1,9 @@
-print('Alo Python')
+N1 = int(input("Digite a primeira nota: "))
+N2 = int(input("Nota 2 "))
+N3 = int(input("Nota 3 "))
+N4 = int(input("Nota 4 "))
+
+MA = (N1 + N2 + N3 + N4)/4
+print(MA)
+
+
---
+++ Exercicio2.py
@@ -0,0 +1,7 @@
+H = float(input("Altura: "))
+R = float(input("Raio: "))
+AREA = (3.14*(R**2))+(2*3.14*R*H)
+LITRO = AREA/3
+QTDE = LITRO/5
+C= QTDE * 50.0
+print(C, QTDE)
